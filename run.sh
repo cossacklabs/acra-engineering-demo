26134398bb749e39769576524457bc3259e29899
Dmytro Shapovalov <shad@shad.in.ua>
2018-09-19 19:54:44 +0300

    Use domain name in description

--- run.sh
+++ run.sh
@@ -115,18 +115,18 @@ Resources that will become available after launch:
         http://www.djangoproject.example:8000/admin
         Default user/password: admin/admin
     * PostgreSQL
-        postgresql://$SERVER_IP:5432
+        postgresql://www.djangoproject.example:5432
         Default admin user/password: postgres/test
     * Web interface for PostgreSQL
-        http://$SERVER_IP:8008
+        http://www.djangoproject.example:8008
         Default user/password: test/test
     * AcraConnector
-        tcp://$SERVER_IP:9494
+        tcp://www.djangoproject.example:9494
     * AcraWebConfig
-        http://$SERVER_IP:8001
+        http://www.djangoproject.example:8001
         Default user/password: test/test
     * Prometheus
-        http://$SERVER_IP:9090
+        http://www.djangoproject.example:9090
 
 '
     read < /dev/tty -n 1 -s -r -p 'Press any key to continue...'
