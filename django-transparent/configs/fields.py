1be34a2dfc2dc4368bbad04cd5431dea9ebecbad
Dmytro Shapovalov <shad@shad.in.ua>
2019-01-02 11:08:57 +0200

    Renamed classes and methods

--- django-transparent/configs/fields.py
+++ django-transparent/configs/fields.py
@@ -3,24 +3,26 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 import codecs
 
-class TextBinaryField(models.TextField):
+
+class PgTextBinaryField(models.TextField):
     description = _("Text as binary data")
 
     def from_db_value(self, value, expression, connection):
-        return dbvalue_to_string(value)
+        return db_value_to_string(value)
 
     def get_db_prep_value(self, value, connection, prepared=False):
         return string_to_dbvalue(value)
 
 
-class CharBinaryField(models.CharField):
+class PgCharBinaryField(models.CharField):
     description = _("Chars as binary data")
 
     def from_db_value(self, value, expression, connection):
-        return dbvalue_to_string(value)
+        return db_value_to_string(value)
 
     def get_db_prep_value(self, value, connection, prepared=False):
         return string_to_dbvalue(value)
+
 
 def bytes_to_string(b):
     if len(b) >= 2 and b[0:2] == b'\\x':
@@ -33,7 +35,7 @@ def memoryview_to_string(mv):
     return bytes_to_string(mv.tobytes())
 
 
-def dbvalue_to_string(value):
+def db_value_to_string(value):
     if isinstance(value, memoryview):
         return memoryview_to_string(value)
     elif isinstance(value, bytes) or isinstance(value, bytearray):
