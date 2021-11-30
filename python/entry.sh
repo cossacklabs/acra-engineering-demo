211ca7e26373c3c7257e266decb082c7187ea3e8
Zhars <95243966+ZhmakaAS@users.noreply.github.com>
2021-11-30 14:20:41 +0200

    Update python/entry.sh
    Co-authored-by: Dmytro Shapovalov <shad@shad.in.ua>

--- python/entry.sh
+++ python/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find $PUBLIC_KEY_DIR -type f -name \*.pub -print > /pub_key_name.txt
+find "$PUBLIC_KEY_DIR" -type f -name \*.pub -print > /pub_key_name.txt
 
 while true; do
     sleep 1
